904a619ba079041793cb0d67b17167c5a744cc45
hadafasa <hadafasa@DESKTOP-C7RN5OR>
2023-04-06 15:59:08 -0500

    added a lot of items and cleaned up some code

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// Scroll Reveal Section
+ScrollReveal ({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.contact form, .tero, .tero head',{origin: 'top'});
+
+// ScrollReveal().reveal('.contact form');
