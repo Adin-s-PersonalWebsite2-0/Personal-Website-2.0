4795db0e3714c90f677f3ea5eb9eee0697ac7cd5
hadafasa <hadafasa@DESKTOP-C7RN5OR>
2023-04-06 20:09:00 -0500

    new changes to almost everything

--- javascript.js
+++ javascript.js
@@ -1,32 +1,32 @@
 // Scroll Reveal Section
-ScrollReveal ({
-    reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-});
+// ScrollReveal ({
+//     reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 200
+// });
 
-ScrollReveal().reveal('.contact form, .tero, .tero head',{origin: 'top'});
+// ScrollReveal().reveal('.contact form, .tero, .tero head',{origin: 'top'});
 
 // ScrollReveal().reveal('.contact form');
 
 const form = document.querySelector("#form")
-   const submitButton = document.querySelector("#submit")
-   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
 
-   form.addEventListener('submit', e => {
-     submitButton.disabled = true
-     e.preventDefault()
-     let requestBody = new FormData(form)
-     fetch(scriptURL, { method: 'POST', body: requestBody})
-       .then(response => {
-          alert('Success!', response)
-          submitButton.disabled = false
-         })
-       .catch(error => {
-       alert('Error!', error.message)
-         submitButton.disabled = false
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+       alert('Success!', response)
+       submitButton.disabled = false
+      })
+    .catch(error => {
+    alert('Error!', error.message)
+      submitButton.disabled = false
 
-       }
-       )
-   })
+    }
+    )
+})
