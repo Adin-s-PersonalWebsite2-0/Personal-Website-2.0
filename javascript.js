0e2bb3e8cba0e867e3840090f62c1479775fc177
hadafasa <hadafasa@DESKTOP-C7RN5OR>
2023-04-10 20:23:58 -0500

    some changes

--- javascript.js
+++ javascript.js
@@ -10,23 +10,56 @@
 
 // ScrollReveal().reveal('.contact form');
 
-const form = document.querySelector("#form")
-const submitButton = document.querySelector("#submit")
-const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+// const form = document.querySelector("#form")
+// const submitButton = document.querySelector("#submit")
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbxLZTlgHBunzjDAyvzTFB9AAQZS2vGT4hSZKlfMDVMQrrHEQwKDYO2-fo05BNfFegYWVg/exec'
 
-form.addEventListener('submit', e => {
-  submitButton.disabled = true
-  e.preventDefault()
-  let requestBody = new FormData(form)
-  fetch(scriptURL, { method: 'POST', body: requestBody})
-    .then(response => {
-       alert('Success!', response)
-       submitButton.disabled = false
-      })
-    .catch(error => {
-    alert('Error!', error.message)
-      submitButton.disabled = false
+// form.addEventListener('submit', e => {
+//   submitButton.disabled = true
+//   e.preventDefault()
+//   let requestBody = new FormData(form)
+//   fetch(scriptURL, { method: 'POST', body: requestBody})
+//     .then(response => {
+//        alert('Success!', response)
+//        submitButton.disabled = false
+//       })
+//     .catch(error => {
+//     alert('Error!', error.message)
+//       submitButton.disabled = false
 
-    }
-    )
-})
+//     }
+//     )
+// })
+
+const form = document.querySelector("form");
+const thankyou = document.querySelector(".thankyou");
+const nameInput = document.querySelector(
+  'input[name="name"]'
+);
+
+let isFormValid = false;
+
+const validateInputs = () => {
+  nameInput.classList.remove("invalid");
+  nameInput.nextElementSibling.classList.add("hidden");
+  isFormValid = true;
+
+  if (!nameInput.value) {
+    nameInput.classList.add("invalid");
+    nameInput.nextElementSibling.classList.remove("hidden");
+    isFormValid = false;
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+  if (isFormValid) {
+    form.remove();
+    thankyou.classList.remove("hidden");
+  }
+});
+
+nameInput.addEventListener("input", () => {
+  validateInputs();
+});
